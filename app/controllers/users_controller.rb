614d5bdf8157d068e2f431857905f039081313c2
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-03-14 17:05:03 +0100

    Skip authentication when creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_request
+  skip_before_action :authenticate_request, only: [:create, :create_and_login]
 
   def create
     @user = User.new(user_params)
